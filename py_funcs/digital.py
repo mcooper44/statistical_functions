d866bb572cb500978b0d6850df628111f0a856dd
Matt Cooper <m.cooper44@gmail.com>
2023-12-27 23:58:15 -0500

    fixed error where var name was inserted to f str
    Didn't add the {}

--- py_funcs/digital.py
+++ py_funcs/digital.py
@@ -5,11 +5,11 @@ from typing import Union
 from s_math import _divmod
 
 
-def int_to_binary(n: Union[int, float]) -> str:
+def int_to_bin(n: Union[int, float]) -> str:
     x = n
     b_str = ''
     while x > 0:
         q, r = _divmod(x, 2)
         x = q
-        b_str = f'{r}b_str'
+        b_str = f'{r}{b_str}'
     return f'0b{b_str}'
