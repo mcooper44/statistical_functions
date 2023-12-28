5bc875fd09ac6ef46fbaa3a60c78fcfea071a7fe
Matt Cooper <m.cooper44@gmail.com>
2023-12-27 23:08:25 -0500

    funcs for digital number conversions
    added function for decimal to binary conversion, returning a str
    representation of the int in binary format

---
+++ py_funcs/digital.py
@@ -0,0 +1,15 @@
+'''
+Functions for working with binary and hex data
+'''
+from typing import Union
+from s_math import _divmod
+
+
+def int_to_binary(n: Union[int, float]) -> str:
+    x = n
+    b_str = ''
+    while x > 0:
+        q, r = _divmod(x, 2)
+        x = q
+        b_str = f'{r}b_str'
+    return f'0b{b_str}'
