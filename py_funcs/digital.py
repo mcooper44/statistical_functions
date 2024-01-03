a096fdf908dd14e57c81355c79daea901429ad91
Matt Cooper <m.cooper44@gmail.com>
2024-01-02 22:48:46 -0500

    added simple bin to int function
    a simple iterative method that cycles through a binary string and
    adds powers*index to an accumulator which is returned
    
    also fixed some errors in the function typing only ints are dealt with
    so far

--- py_funcs/digital.py
+++ py_funcs/digital.py
@@ -1,11 +1,10 @@
 '''
 Functions for working with binary and hex data
 '''
-from typing import Union
 from s_math import _divmod
 
 
-def int_to_bin(n: Union[int, float]) -> str:
+def int_to_bin(n: int) -> str:
     x = n
     b_str = ''
     while x > 0:
@@ -13,3 +12,13 @@ def int_to_bin(n: Union[int, float]) -> str:
         x = q
         b_str = f'{r}{b_str}'
     return f'0b{b_str}'
+
+
+def bin_to_int(n: str) -> int:
+    v = 0
+    i = 0
+    for _ in range(len(n)):
+        _i = int(n[::-1][i])
+        v += ((2**i)*_i)
+        i += 1
+    return v
