ad33ef14611545e8f9e7c2f081f2858b946ae0fd
Matt Cooper <m.cooper44@gmail.com>
2024-11-16 16:00:46 -0500

    bin2hex + hex2bin dicts at file head

--- py_funcs/digital.py
+++ py_funcs/digital.py
@@ -4,6 +4,21 @@ Functions for working with binary and hex data
 from s_math import _divmod
 from s_math import pad_to_base
 
+
+B2H_LU = {
+        '0000': '0', '0001': '1', '0010': '2', '0011': '3',
+        '0100': '4', '0101': '5', '0110': '6', '0111': '7',
+        '1000': '8', '1001': '9', '1010': 'A', '1011': 'B',
+        '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'
+    }
+
+H2B_LU = {
+         '0': '0000', '1': '0001', '2': '0010', '3': '0011',
+         '4': '0100', '5': '0101', '6': '0110', '7': '0111',
+         '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
+         'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
+    }
+
 def int_to_bin(n: int, flag: bool = False) -> str:
     x = n
     b_str = ''
@@ -25,15 +40,9 @@ def bin_to_int(n: str) -> int:
 
 
 def int_to_hex(n: int, flag: bool = False) -> str:
-    lu = {
-        '0000': '0', '0001': '1', '0010': '2', '0011': '3',
-        '0100': '4', '0101': '5', '0110': '6', '0111': '7',
-        '1000': '8', '1001': '9', '1010': 'A', '1011': 'B',
-        '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'
-    }
     bin_val = int_to_bin(n)
     pad = pad_to_base(len(bin_val), 4)
     bin_str = f'{'0'*pad}{bin_val}'
-    bin_seg = [bin_str[i:i+4] for i in range(0, len(bin_str), 4)] 
-    hex_val = ''.join(lu.get(s, '') for s in bin_seg)
+    bin_seg = [bin_str[i:i+4] for i in range(0, len(bin_str), 4)]
+    hex_val = ''.join(B2H_LU.get(s, '') for s in bin_seg)
     return f'0x{hex_val}' if flag else hex_val
