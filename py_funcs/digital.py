8b2d3c923c9a3d482c51b46a08edd8848a6ce5f8
Matt Cooper <m.cooper44@gmail.com>
2024-11-16 20:17:17 -0500

    bin2hex + hex2int funcs

--- py_funcs/digital.py
+++ py_funcs/digital.py
@@ -3,8 +3,9 @@ Functions for working with binary and hex data
 '''
 from s_math import _divmod
 from s_math import pad_to_base
+from s_math import _sum
 
-
+# Binary to Hex lookup
 B2H_LU = {
         '0000': '0', '0001': '1', '0010': '2', '0011': '3',
         '0100': '4', '0101': '5', '0110': '6', '0111': '7',
@@ -12,12 +13,17 @@ B2H_LU = {
         '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'
     }
 
+# Hex to binary lookup
 H2B_LU = {
          '0': '0000', '1': '0001', '2': '0010', '3': '0011',
          '4': '0100', '5': '0101', '6': '0110', '7': '0111',
          '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
          'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
     }
+
+HEX_VALUES = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
+              '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
 
 def int_to_bin(n: int, flag: bool = False) -> str:
     x = n
@@ -47,5 +53,17 @@ def int_to_hex(n: int, flag: bool = False) -> str:
     hex_val = ''.join(B2H_LU.get(s, '') for s in bin_seg)
     return f'0x{hex_val}' if flag else hex_val
 
+
 def hex_to_bin(h: str) -> str:
     return ''.join(H2B_LU.get(char, '') for char in h)
+
+
+def bin_to_hex(b: str) -> str:
+    return int_to_hex(bin_to_int(b))
+
+
+def hex_to_int(h: str) -> int:
+    multiplier = [int(HEX_VALUES.index(v)) for v in h[::-1]]
+    return _sum(m*(16**p) for m,p in zip(multiplier, range(0, len(h))))
+
+
